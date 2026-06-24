a4c50c0e348c7ae41d3c80a652ac3a99ea6f74f3
preampareesh <preampareesh@gmail.com>
2026-06-24 17:26:23 +0530

    Time: 808 ms (13.26%) | Memory: 42.9 MB (55.23%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -3,18 +3,25 @@ class Solution {
         int n=s.length();
         int count =0;
         for(int i=0; i<n; i++){
-            count+=findpal(s,i,i);
-            count+=findpal(s,i,i+1);
-        }
-        return count ;
-    }
-    private int findpal(String s, int i, int j){
-         int count =0;
-        while(i>=0 && j<s.length() && s.charAt(i) == s.charAt(j)){
-            count ++;
-            i--;
-            j++;
+            for(int j=i; j<n; j++){
+                if(findpal(s,i,j)){
+                    count ++;
+                }
+            }
         }
         return count;
+        
+    }
+    private boolean findpal(String s, int i, int j){
+        while(i<j){
+        if(s.charAt(i) != s.charAt(j)){
+                return false;
+     
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
     }
 }
